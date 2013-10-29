975d99c39b90d49fe362ee1a7bb87758d84952eb
Gustavo Quandt <gustavoquandt@gmail.com>
2013-10-29 19:48:00 -0200

    Teste
    Teste

--- ASPMVCTrabalho/App_Start/AuthConfig.cs
+++ ASPMVCTrabalho/App_Start/AuthConfig.cs
@@ -26,6 +26,7 @@ namespace ASPMVCTrabalho
             //    appId: "",
             //    appSecret: "");
 
+            //Test
             OAuthWebSecurity.RegisterGoogleClient();
         }
     }
