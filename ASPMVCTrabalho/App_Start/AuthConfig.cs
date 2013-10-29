b06c6c9eec3ab03d4e6347a5b421fdc2d7c31ae2
Gustavo Quandt <gustavoquandt@gmail.com>
2013-10-29 20:28:38 -0200

    Remover Teste
    Teste

--- ASPMVCTrabalho/App_Start/AuthConfig.cs
+++ ASPMVCTrabalho/App_Start/AuthConfig.cs
@@ -26,8 +26,7 @@ namespace ASPMVCTrabalho
             //    appId: "",
             //    appSecret: "");
 
-            //test
-
+            
             OAuthWebSecurity.RegisterGoogleClient();
         }
     }
